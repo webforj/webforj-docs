2923d1cd501f2004c0779e0f6bbe9f44d9eacce5
Garrison Osteen <gosteen@basis.cloud>
2024-09-24 12:41:15 -0600

    Added all doc component files to the global scope, so we don't have to import them on every page.

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,27 @@
+import React from 'react';
+// Import Components here and add to the export list to include in global scope
+
+import MDXComponents from '@theme-original/MDXComponents';
+import AppLayoutViewer from '@site/src/components/DocsTools/AppLayoutViewer';
+import ComponentBasics from '@site/src/components/DocsTools/ComponentBasics';
+import ComponentCard from '@site/src/components/DocsTools/ComponentCard';
+import ComponentDemo from '@site/src/components/DocsTools/ComponentDemo';
+import DocChip from '@site/src/components/DocsTools/DocChip';
+import EventTable from '@site/src/components/DocsTools/EventTable';
+import JavadocLink from '@site/src/components/DocsTools/JavadocLink';
+import TableBuilder from '@site/src/components/DocsTools/TableBuilder';
+import TabSwitcher from '@site/src/components/DocsTools/TabSwitcher';
+
+export default {
+  ...MDXComponents,
+  AppLayoutViewer,
+  ComponentBasics,
+  ComponentCard,
+  ComponentDemo,
+  DocChip,
+  EventTable,
+  JavadocLink,
+  TableBuilder,
+  TabSwitcher,
+};
+
