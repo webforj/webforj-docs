2fe21d2673c98261107b2252d5d825853f590e2d
MatthewHawkins <matthew.hawkins1115@gmail.com>
2024-10-15 09:48:45 +0200

    chore: fixed vale warning, added image and updated banner

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -90,9 +90,9 @@ const config = {
         //... other Algolia params
       },
       announcementBar: {
-        id: '24.11',
+        id: '24.12',
         content:
-          'We are excited to announce webforJ version 24.11 is live! Read more about the changes and features <a href=/blog/whats-new-v24.11>here.</a> ',
+          'We are excited to announce webforJ version 24.12 is live! Read more about the changes and features <a href=/blog/whats-new-v24.12>here.</a> ',
         // backgroundColor: 'var(--announcement-background)',
         // textColor: '#ebedf0',
         // backgroundColor: 'var(--chip-background)',
